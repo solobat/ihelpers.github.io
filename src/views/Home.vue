a2e8f33ca8e23aac76b8e24da6ccf137505b6429
tomasy <solopea@gmail.com>
2020-06-11 15:27:10 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    Steward Helper
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
