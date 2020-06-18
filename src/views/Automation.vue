891bc87f46d9c9fde24fb79ffc36488f1c9375e1
tomasy <solopea@gmail.com>
2020-06-18 23:21:19 +0800

    update: bugfix

--- src/views/Automation.vue
+++ src/views/Automation.vue
@@ -113,7 +113,7 @@ export default {
         repo: 'Steward-Helper-Comments',
         owner: 'solobat',
         admin: ['solobat'],
-        id: location.pathname,      // Ensure uniqueness and length less than 50
+        id: this.$route.path,      // Ensure uniqueness and length less than 50
         distractionFreeMode: false
       })
 
